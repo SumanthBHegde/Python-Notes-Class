eaba15926d5815b3aae2b33de05791df66287856
SumanthBHegde <sumanthhegde002@gmail.com>
2024-10-29 14:38:14 +0530

    sec 7

--- bruteforece_sec7.py
+++ bruteforece_sec7.py
@@ -1,25 +1,23 @@
-# Bruteforece attack
 import itertools
 import string
 
-def bruteforec (password):
+def bruteforce_attack(password):
     chars = string.printable.strip()
+    # print(chars)
     attempts = 0
     for length in range(len(password), len(password) + 1):
-        for guess in itertools.product(chars,repeat=length):
+        for guess in itertools.product(chars, repeat=length):
             attempts += 1
             guess = ''.join(guess)
             if guess == password:
                 return (attempts, guess)
-        return(attempts, None)
-    
-def main():
-    password = input('input the passowrd to crack: ')
-    attempts, guess = bruteforec(password) 
-    if guess:
-        print(f'Password cracked in {attempts} attempts. The password is {guess}')
-    else:
-        print(f'Password no cracked after {attempts} attempts .')   
-    
-    
-main()
+    return(attempts, None)
+
+# Main Program
+password = input("Input the password to crack: ")
+attempts, guess = bruteforce_attack(password)
+
+if guess:
+    print(f"Password cracked in {attempts} attempts. The password is {guess}")
+else:
+    print(f"Password not cracked after {attempts} attempts.")
